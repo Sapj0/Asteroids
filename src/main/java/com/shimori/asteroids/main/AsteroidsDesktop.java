927f216bcbfcb73fb0a2047df7c1fdd40ac3d54a
Shimori <aschlumb@gmail.com>
2014-11-27 00:52:05 +0100

    2nd commit
    mise à niveau des fichiers et des classes du projet

--- src/main/java/com/shimori/asteroids/main/AsteroidsDesktop.java
+++ src/main/java/com/shimori/asteroids/main/AsteroidsDesktop.java
@@ -1,5 +1,20 @@
 package com.shimori.asteroids.main;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class AsteroidsDesktop {
+	
+	public static void main(String[] args){
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		
+		cfg.title = "Asteroids";
+		cfg.width = 500;
+		cfg.height = 400;
+		cfg.useGL20 = false;
+		cfg.resizable = false;
+		
+		new LwjglApplication(new Game(), cfg);
+	}
 
 }
--- src/main/java/com/shimori/asteroids/main/Game.java
+++ src/main/java/com/shimori/asteroids/main/Game.java
@@ -1,5 +1,53 @@
 package com.shimori.asteroids.main;
 
-public class Game {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.shimori.asteroids.managers.GameInputProcessor;
+import com.shimori.asteroids.managers.GameKeys;
+
+
+public class Game implements ApplicationListener{
+	
+	public static int WIDTH;
+	public static int HEIGHT;
+	
+	public static OrthographicCamera camera;
+	
+	public void create(){
+		
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera(WIDTH, HEIGHT);
+		camera.translate(WIDTH / 2, HEIGHT / 2);
+		camera.update();
+		
+		Gdx.input.setInputProcessor(new GameInputProcessor());
+	}
+	
+	public void render(){
+		
+		// clear screen black
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		if (GameKeys.isPressed(GameKeys.SPACE)) {
+			System.out.println("SPACE");
+		}
+		
+		if (GameKeys.isPressed(GameKeys.ESCAPE)) {
+			System.out.println("ESCAPE");
+		}
+		
+		GameKeys.update();
+		
+	}
+	
+	public void resize(int width, int height){}
+	public void pause(){}
+	public void resume(){}
+	public void dispose(){}
 
 }
